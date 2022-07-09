05dca6f6ac91de42b71340007c30c1f6c45b3ad0
zbyju <zbynek.jurica@gmail.com>
2022-07-09 21:08:09 +0200

    Add basic routes and logging

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log("test", 1);
+import * as dotenv from "dotenv";
+import * as express from "express";
+import * as morgan from "morgan";
+import { router as engineRoute } from "./routes/engineRoute";
+import { router as thermalRoute } from "./routes/thermalRoute";
+
+async function main() {
+  // .env file load
+  dotenv.config();
+
+  const app = express();
+  const port = process.env.PORT || 5000;
+
+  // Setup logger - morgan
+  app.use(morgan("dev"));
+
+  //Routes
+  app.use(process.env.API_PREFIX + "/engine", engineRoute);
+  app.use(process.env.API_PREFIX + "/thermal", thermalRoute);
+
+  app.listen(port, () => {
+    console.log(`Babybox backed listening on port ${port}`);
+  });
+}
+
+main();
---
+++ src/routes/engineRoute.ts
@@ -0,0 +1,7 @@
+import * as express from "express";
+
+export const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send({ msg: "test" });
+});
